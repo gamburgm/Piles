64c116a9e574a422339fe54dc827b33a5e890d14
asikarov <asikarov@gmail.com>
2020-07-18 20:50:55 -0400

    all in a day's work

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class Banana extends Component {
+    render() {
+        return (
+            <div>
+                <p>Banana grams 1</p>
+                <p>Banana grams 2</p>
+                <p>Banana grams 3</p>
+                <p>Banana grams 4</p>
+            </div>
+        );
+    }
+}
 
 export default function App() {
     return (
-        <>
-        <h1 onClick={alert('Hi user!')}>ooga booga</h1>
-        <h2>chungus</h2>
-        </>
-
+        <div>
+            <h1 onClick={alert('Hi user!')}>ooga booga</h1>
+            <h2>chungus</h2>
+            <Banana/>
+        </div>
     );
 }
